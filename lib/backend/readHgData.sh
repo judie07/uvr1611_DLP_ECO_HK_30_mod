805dd8550589465fa6744b691eba85faf41abfeb
sony <sony@risup.net>
2018-03-18 21:42:20 +0100

    minor

--- lib/backend/readHgData.sh
+++ lib/backend/readHgData.sh
@@ -11,9 +11,9 @@ if [[ $DATE != $LAST_INSERT ]]
 then
 #echo $line | sed "s/pm /null,\\'$DATE\\',/"| sed -r 's/\s+/,/g' |  sed -re "s/([0-9A-F]{4}),/unhex(\\'\1\\'),/g"   | sed 's/.*/echo "insert ignore into t_hg_data values (&)"/e'| sed 's/,)/,null);/' | mysql -u uvr1611 --password="uvr1611" --database="uvr1611"
 #echo $line
-echo $line | sed -n -e 's/^pm/pm/p' |  sed "s/pm /null,\'$DATE\',/"| sed -r 's/\s+/,/g' | sed -re "s/,([0-9a-fA-F]+)/,unhex(\'\1\')/179g" | sed 's/.*/echo "insert ignore into t_hg_data_new values (&)"/e'| sed 's/,)/,null);/' 
+echo $line | sed -n -e 's/^pm/pm/p' |  sed "s/pm /null,\'$DATE\',/"| sed -r 's/\s+/,/g' | sed -re "s/,([0-9a-fA-F]+)/,unhex(\'\1\')/195g" | sed 's/.*/echo "insert ignore into t_hg_data_new values (&)"/e'| sed 's/,)/,null);/' 
 
-echo $line | sed -n -e 's/^pm/pm/p' |  sed "s/pm /null,\'$DATE\',/"| sed -r 's/\s+/,/g' | sed -re "s/,([0-9a-fA-F]+)/,unhex(\'\1\')/179g" | sed 's/.*/echo "insert ignore into t_hg_data_new values (&)"/e'| sed 's/,)/,null);/' | mysql -u uvr1611 --password="uvr1611" --database="uvr1611"
+echo $line | sed -n -e 's/^pm/pm/p' |  sed "s/pm /null,\'$DATE\',/"| sed -r 's/\s+/,/g' | sed -re "s/,([0-9a-fA-F]+)/,unhex(\'\1\')/195g" | sed 's/.*/echo "insert ignore into t_hg_data_new values (&)"/e'| sed 's/,)/,null);/' | mysql -u uvr1611 --password="uvr1611" --database="uvr1611"
 
 #echo $line | sed "s/pm /null,\\'$DATE\\',/"| sed -r 's/\s+/,/g' | sed -re "s/,([0-9a-fA-F])/,unhex(\'\1\')/88g" |         sed 's/.*/echo "insert into t_hg_data values (&)"/e'| sed 's/,)/,null);/' 
 LAST_INSERT=$DATE
